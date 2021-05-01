cc99b9e292ba117ce512078728bbeb91360dfdc8
zzacong <ongzz100@gmail.com>
2021-05-01 22:01:12 +1000

    feat: webhook events

--- src/api/webhooks.ts
+++ src/api/webhooks.ts
@@ -1,16 +1,58 @@
 import { Request, Response } from 'express'
+import { firestore } from 'firebase-admin'
 import Stripe from 'stripe'
 import { stripe } from '../'
+import { db } from '../firebase'
 
 //  * Business logic for specific webhook event types
 const webhookHandlers: WebhookHandlers = {
+  'checkout.session.completed': async (data: Stripe.Event.Data) => {
+    console.log('[WEBHOOK] checkout.session.completed')
+    // TODO: Add your business logic here
+  },
   'payment_intent.succeeded': async (data: Stripe.PaymentIntent) => {
-    console.log('[payment_intent.succeeded]')
+    console.log('[WEBHOOK] payment_intent.succeeded')
     // TODO: Add your business logic here
   },
   'payment_intent.payment_failed': async (data: Stripe.PaymentIntent) => {
-    console.log('[payment_intent.payment_failed]')
+    console.log('[WEBHOOK] payment_intent.payment_failed')
     // TODO: Add your business logic here
+  },
+  'customer.subscription.deleted': async (data: Stripe.Subscription) => {
+    console.log('[WEBHOOK] customer.subscription.deleted')
+    const customer = (await stripe.customers.retrieve(
+      data.customer as string
+    )) as Stripe.Customer
+    const userId = customer.metadata.firebaseUID
+    const userRef = db.collection('users').doc(userId)
+
+    await userRef.update({
+      activePlans: firestore.FieldValue.arrayRemove(data.items.data[0].plan.id),
+    })
+  },
+  'customer.subscription.created': async (data: Stripe.Subscription) => {
+    console.log('[WEBHOOK] customer.subscription.created')
+    const customer = (await stripe.customers.retrieve(
+      data.customer as string
+    )) as Stripe.Customer
+    const userId = customer.metadata.firebaseUID
+    const userRef = db.collection('users').doc(userId)
+
+    await userRef.update({
+      activePlans: firestore.FieldValue.arrayUnion(data.items.data[0].plan.id),
+    })
+  },
+  'invoice.payment_succeeded': async (data: Stripe.Invoice) => {
+    console.log('[WEBHOOK] invoice.payment_succeeded')
+    // TODO: Add your business logic here
+  },
+  'invoice.payment_failed': async (data: Stripe.Invoice) => {
+    console.log('[WEBHOOK] invoice.payment_failed')
+    const customer = (await stripe.customers.retrieve(
+      data.customer as string
+    )) as Stripe.Customer
+    const userRef = db.collection('users').doc(customer.metadata.firebaseUID)
+    await userRef.update({ status: 'PAST_DUE' })
   },
 }
 
